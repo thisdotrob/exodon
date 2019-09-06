076b4dae4a8b59387e2f619689ed50a7f8d52125
Rob Stevenson <rs@gower.st>
2019-09-06 21:57:12 +0100

    Retrieve list of transactions from Starling
    Also use proper Structs for unmarshalling json from http responses.

--- sync_starling.go
+++ sync_starling.go
@@ -19,34 +19,106 @@ func SyncStarling(ctx context.Context, m PubSubMessage) error {
 
 	client := &http.Client{}
 
-	req, err := http.NewRequest("GET", "https://api.starlingbank.com/api/v2/accounts", nil)
-
 	authHeader := fmt.Sprintf("Bearer %s", token)
 
-	req.Header.Add("Authorization", authHeader)
+	accounts_req, err := http.NewRequest("GET", "https://api.starlingbank.com/api/v2/accounts", nil)
+	accounts_req.Header.Add("Authorization", authHeader)
 
-	resp, err := client.Do(req)
+	accounts_resp, err := client.Do(accounts_req)
 
 	if err != nil {
-		log.Fatal("Error making request to Starling.\n[ERRO] -", err)
+		log.Fatal("Error making accounts request to Starling.\n[ERRO] -", err)
 	}
 
-	defer resp.Body.Close()
+	defer accounts_resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	if accounts_resp.StatusCode != 200 {
+		log.Fatal("Non 200 making accounts request to Starling.\n[CODE] -", accounts_resp.StatusCode)
+	}
 
-	var dat map[string]interface{}
+	accounts_body, _ := ioutil.ReadAll(accounts_resp.Body)
 
-	if err := json.Unmarshal(body, &dat); err != nil {
+	type Account struct {
+		Account_uid      string `json:"accountUid"`
+		Default_category string `json:"defaultCategory"`
+	}
+
+	type Accounts struct {
+		Accounts []Account `json:"accounts"`
+	}
+
+	var accounts_dat Accounts
+
+	if err := json.Unmarshal(accounts_body, &accounts_dat); err != nil {
 		panic(err)
 	}
 
-	accounts := dat["accounts"].([]interface{})
-	account := accounts[0].(map[string]interface{})
-	accountUid := account["accountUid"].(string)
-	defaultCategory := account["defaultCategory"].(string)
-	log.Printf("accountUid: %s", accountUid)
-	log.Printf("defaultCategory: %s", defaultCategory)
+	account_uid := accounts_dat.Accounts[0].Account_uid
+	default_category := accounts_dat.Accounts[0].Default_category
+
+	last_synced := "2017-09-06T00:00:00.000Z"
+
+	transactions_req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("https://api.starlingbank.com/api/v2/feed/account/%s/category/%s?changesSince=%s", account_uid, default_category, last_synced),
+		nil,
+	)
+	transactions_req.Header.Add("Authorization", authHeader)
+	transactions_resp, err := client.Do(transactions_req)
+
+	if err != nil {
+		log.Fatal("Error making transactions request to Starling.\n[ERRO] -", err)
+	}
+
+	defer transactions_resp.Body.Close()
+
+	if transactions_resp.StatusCode != 200 {
+		log.Fatal("Non 200 making transactions request to Starling.\n[CODE] -", transactions_resp.StatusCode)
+	}
+
+	transactions_body, _ := ioutil.ReadAll(transactions_resp.Body)
+
+	type Amount struct {
+		Currency    string `json:"currency"`
+		Minor_units int    `json:"minorUnits"`
+	}
+
+	type Transaction struct {
+		Feed_item_uid                           string `json:"feedItemUid"`
+		Category_uid                            string `json:"categoryUid"`
+		Amount                                  Amount `json:"amount"`
+		Source_amount                           Amount `json:"sourceAmount"`
+		Direction                               string `json:"direction"`
+		Updated_at                              string `json:"updatedAt"`
+		Transaction_time                        string `json:"transactionTime"`
+		Settlement_time                         string `json:"settlementTime"`
+		Source                                  string `json:"source"`
+		Source_sub_type                         string `json:"sourceSubType"`
+		Status                                  string `json:"status"`
+		Counter_party_Type                      string `json:"counterPartyType"`
+		Counter_party_uid                       string `json:"counterPartyUid"`
+		Counter_party_Name                      string `json:"counterPartyName"`
+		Counter_party_sub_entity_uid            string `json:"counterPartySubEntityUid"`
+		Counter_party_sub_entity_name           string `json:"counterPartySubEntityName"`
+		Counter_party_sub_entity_identifier     string `json:"counterPartySubEntityIdentifier"`
+		Counter_party_sub_entity_sub_identifier string `json:"counterPartySubEntitySubIdentifier"`
+		Reference                               string `json:"reference"`
+		Country                                 string `json:"country"`
+		Spending_category                       string `json:"spendingCategory"`
+		User_note                               string `json:"userNote"`
+	}
+
+	type Transactions struct {
+		Feed_items []Transaction `json:"feedItems"`
+	}
+
+	var transactions Transactions
+
+	if err := json.Unmarshal(transactions_body, &transactions); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(transactions.Feed_items[0])
 
 	return nil
 }
